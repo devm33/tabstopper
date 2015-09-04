986aeba06152a511f98f488b78c222f82cfd8fda
Devraj Mehta <devm33@gmail.com>
2015-09-04 01:42:51 -0400

    updating test code

--- background.js
+++ background.js
@@ -1,18 +1,16 @@
-function onBeforeNavigate(req) {
-    /* schema for pending request: {
-     *  tabId
-     *  url
-     *  ...
-     * }
-     * Note: this doesn't stop the navigation to the url
-     */
-    tabQuery({ url: req.url })
+function onUpdated(tabId, change) {
+    if(!change.url) {
+        // url not updating
+        return;
+    }
+    console.log('You\'re going to', change.url, 'in tab', tabId);
+    tabQuery({ url: change.url })
     .then(function(tabs) {
-        console.log('You went to', req.url);
+        console.log('You went to', change.url);
         console.log('It is open in', tabs);
     });
 }
 
-chrome.webNavigation.onBeforeNavigate.addListener(onBeforeNavigate);
+chrome.tabs.onUpdated.addListener(onUpdated);
 
 var tabQuery = Q.denodeify(chrome.tabs.query);
