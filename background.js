cfa1dcd8fd0236d354667dabe624bf34d22d5999
Devraj Mehta <devm33@gmail.com>
2015-09-06 02:15:34 -0400

    adding logic to classify type of tab updates

--- background.js
+++ background.js
@@ -1,16 +1,27 @@
 function onUpdated(tabId, change) {
     if(!change.url) {
-        // url not updating
+        // url not changed in tab update
         return;
     }
-    console.log('You\'re going to', change.url, 'in tab', tabId);
-    tabQuery({ url: change.url })
-    .then(function(tabs) {
-        console.log('You went to', change.url);
-        console.log('It is open in', tabs);
+
+
+    var urlWithoutHash = change.url.split('#')[0];
+    var baseUrl = urlWithoutHash.split('?')[0];
+    console.log('You\'re going to', baseUrl, 'in tab', tabId);
+
+    chrome.tabs.query({ url: baseUrl + '*' }, function(tabs) {
+        var matches = _.reject(tabs, {id: tabId});
+        var exacts = _.filter(matches, {url: change.url});
+        var matchesExceptHash = _.filter(matches, (tab) => {
+            return tab.url.split('#')[0] === urlWithoutHash;
+        });
+
+        console.log('baseUrl', baseUrl, 'original tab:', tabId, '\n',
+                    'tabs matching:', _.map(matches, 'id'), matches, '\n',
+                    'exact matches:', _.map(exacts, 'id'), exacts, '\n',
+                    'matches without hash:', _.map(matchesExceptHash, 'id'),
+                    matchesExceptHash, '\n');
     });
 }
 
 chrome.tabs.onUpdated.addListener(onUpdated);
-
-var tabQuery = Q.denodeify(chrome.tabs.query);
