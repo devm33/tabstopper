1681ef0466573597800d6b8421668e3e04aa826a
Devraj Mehta <devm33@gmail.com>
2015-09-06 02:25:02 -0400

    only logging if matches found

--- background.js
+++ background.js
@@ -1,26 +1,32 @@
-function onUpdated(tabId, change) {
+function onUpdated(tabId, change, changedTab) {
     if(!change.url) {
         // url not changed in tab update
         return;
     }
 
-
     var urlWithoutHash = change.url.split('#')[0];
     var baseUrl = urlWithoutHash.split('?')[0];
-    console.log('You\'re going to', baseUrl, 'in tab', tabId);
 
     chrome.tabs.query({ url: baseUrl + '*' }, function(tabs) {
         var matches = _.reject(tabs, {id: tabId});
+
+        if(_.isEmpty(matches)) {
+            // no matching open tabs at broadest level
+            return;
+        }
+
         var exacts = _.filter(matches, {url: change.url});
         var matchesExceptHash = _.filter(matches, (tab) => {
             return tab.url.split('#')[0] === urlWithoutHash;
         });
 
-        console.log('baseUrl', baseUrl, 'original tab:', tabId, '\n',
-                    'tabs matching:', _.map(matches, 'id'), matches, '\n',
-                    'exact matches:', _.map(exacts, 'id'), exacts, '\n',
-                    'matches without hash:', _.map(matchesExceptHash, 'id'),
-                    matchesExceptHash, '\n');
+        console.log('original tab:', tabId, changedTab, '\n',
+                    'url:', change.url, '\n',
+                    'baseUrl:', baseUrl, '\n',
+                    'matches base:', _.map(matches, 'id'), matches, '\n',
+                    'matches except hash:', _.map(matchesExceptHash, 'id'),
+                    matchesExceptHash, '\n',
+                    'exact matches:', _.map(exacts, 'id'), exacts, '\n');
     });
 }
 
