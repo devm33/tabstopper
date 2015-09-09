2f7f511310a7bfc0633aa9a765ac8c606615ac30
Devraj Mehta <devm33@gmail.com>
2015-09-08 22:43:37 -0400

    added common library for rules

---
+++ rules.js
@@ -0,0 +1,36 @@
+function loadRules(callback) {
+    chrome.storage.sync.get('rules', (storage) => {
+        if(!storage.rules) {
+            storage.rules = {};
+        }
+        callback(storage.rules);
+    });
+}
+
+function saveRules(rules) {
+    chrome.storage.sync.set({rules: rules});
+}
+
+function matchRule(baseUrl, callback) {
+    loadRules((rules) => {
+        var match = _(rules)
+            .pairs()
+            .filter((rule) => baseUrl.indexOf(rule[0]) > -1)
+            .reduce((a, b) => {
+                // apply most specific rule (longest)
+                if(a[0].length > b[0].length) {
+                    return a;
+                }
+                return b;
+            });
+
+        if(match) {
+            callback({
+                url: match[0],
+                match: match[1]
+            });
+        } else {
+            callback();
+        }
+    });
+}
