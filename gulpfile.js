2e558b6d47b27ef956adb7e768b972f05f116e13
Devraj Mehta <devm33@gmail.com>
2015-09-19 04:10:18 -0400

    add plumber for proper error handling add babel becuase uglify was choking on arrow functions remove debug code

--- gulpfile.js
+++ gulpfile.js
@@ -12,30 +12,31 @@ gulp.task('copy', () => gulp.src([
     .pipe(gulp.dest(dest)));
 
 gulp.task('html', () => gulp.src('src/*.html', base)
+    .pipe($.plumber())
     .pipe($.htmlReplace({js: 'lib.js'}))
     .pipe(gulp.dest(dest)));
 
 gulp.task('js', () => gulp.src('src/*.js', base)
+    .pipe($.plumber())
+    .pipe($.babel())
     .pipe($.ngAnnotate())
     .pipe($.uglify())
     .pipe(gulp.dest(dest)));
-
-
-var d = require('gulp-debug');
 
 gulp.task('lib', () => merge(
     gulp.src(['src/bower/angular/angular.min.js',
              'src/bower/lodash/lodash.min.js']),
     gulp.src('src/templates/*.html', base)
-    .pipe(d({title:'should have zero'}))
+    .pipe($.plumber())
     .pipe($.angularTemplatecache()),
     gulp.src('src/common/*.js')
-    .pipe(d({title:'should have common'}))
+    .pipe($.plumber())
+    .pipe($.babel())
     .pipe($.ngAnnotate()))
+    .pipe($.plumber())
     .pipe($.order(['bower/*', '**/*']))
     .pipe($.concat('lib.js'))
-    // .pipe($.uglify())
-    .pipe(d({title:'should have lib.js'}))
+    .pipe($.uglify())
     .pipe(gulp.dest(dest)));
 
 gulp.task('default', ['copy', 'html', 'js', 'lib']);
