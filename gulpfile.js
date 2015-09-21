a4b730ce3429321c1792a764937a0f657430c082
Devraj Mehta <devm33@gmail.com>
2015-09-20 23:41:12 -0400

    using nifty gulp-dom-src plugin to read in deps from html files! everything working up to angular templates

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,14 @@
+var d = require('gulp-debug');
+
 /*jshint node:true*/
 var gulp = require('gulp');
+
 var $ = require('gulp-load-plugins')();
-var series = require('stream-series');
-var dest = 'extension';
 var base = {base: 'src/'};
+var dest = 'extension';
+var glob = require('glob');
+var lazypipe = require('lazypipe');
+var merge = require('merge-stream');
 
 gulp.task('copy', () => gulp.src([
     'src/manifest.json',
@@ -13,33 +18,52 @@ gulp.task('copy', () => gulp.src([
 
 gulp.task('html', () => gulp.src('src/*.html', base)
     .pipe($.plumber())
-    .pipe($.htmlReplace({js: 'lib.js'}))
+    .pipe($.htmlReplace({js: {src: null, tpl: '<script src="%f.js"></script>'}}))
     .pipe($.htmlmin())
     .pipe(gulp.dest(dest)));
 
-gulp.task('js', () => gulp.src('src/*.js', base)
-    .pipe($.plumber())
-    .pipe($.babel())
-    .pipe($.ngAnnotate())
-    .pipe($.uglify())
-    .pipe(gulp.dest(dest)));
+var js = lazypipe()
+    .pipe($.plumber)
+    .pipe(d, {title: 'js'})
+    .pipe($.babel)
+    .pipe($.ngAnnotate)
+    .pipe($.uglify);
 
-gulp.task('lib', () => series(
-    gulp.src(['src/bower/angular/angular.min.js',
-                'src/bower/lodash/lodash.min.js']),
-    gulp.src('src/templates/*.html', base)
-        .pipe($.plumber())
-        .pipe($.htmlmin())
-        .pipe($.angularTemplatecache()),
-    gulp.src('src/common/*.js')
-        .pipe($.plumber())
-        .pipe($.babel())
-        .pipe($.ngAnnotate()))
-    .pipe($.plumber())
-    .pipe($.concat('lib.js'))
-    .pipe($.uglify())
-    .pipe(gulp.dest(dest)));
+var bower = lazypipe()
+    .pipe($.plumber)
+    .pipe(d, {title: 'bower'})
+    .pipe($.uglify, {compress: false});
 
-gulp.task('default', ['copy', 'html', 'js', 'lib']);
+var templates = lazypipe()
+    .pipe($.plumber)
+    .pipe($.htmlmin)
+    .pipe($.angularTemplatecache);
+
+
+var addTemplates = lazypipe()
+    .pipe($.plumber)
+    .pipe(d, {title: 'before'})
+    .pipe($.addSrc, 'src/common/**/*.html')
+    .pipe(d, {title: 'after'})
+    .pipe(() => $.if('html$', templates()));
+
+gulp.task('js', (done) => glob('src/*.html', (err, files) => {
+    if(err) {
+        done(err);
+    }
+    merge(files.map((file) => $.domSrc({
+        file:file, selector:'script', attribute: 'src', cwd: 'src/'
+    })
+        .pipe($.plumber())
+        .pipe($.if(/bower/, bower(), js()))
+        // .pipe($.if('src/bower/angular', addTemplates()))
+        .pipe(d({title:'before concat '+file}))
+        .pipe($.concat(file))
+        .pipe($.rename({dirname:'', extname:'.js'}))
+        .pipe(gulp.dest(dest))))
+    .on('end', done);
+}));
+
+gulp.task('default', ['copy', 'html', 'js']);
 
 gulp.task('clean', () => require('del')(dest));
