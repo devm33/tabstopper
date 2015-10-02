528f61c38b15fbbeb9e29c3642a23a5b79bc8b10
Devraj Mehta <devm33@gmail.com>
2015-10-01 22:24:57 -0400

    debug wip

--- src/common/notification/notification.js
+++ src/common/notification/notification.js
@@ -9,16 +9,25 @@ angular.module('notification', [])
             var hideNotification = () => $scope.show = false;
             var finishTimeout = () => timeout = false;
             $scope.$watch('show', (show) => {
+                console.log('watch triggered, show = ', show, 'timeout', timeout);
                 if(show) {
+                    // Oh I see the problem is that when we try to set show =
+                    // true temporarily we end up back here with show and
+                    // timeout both truthy
                     if(timeout) {
+
+                        console.log('timeout exists canceling');
                         $timeout.cancel(timeout);
                         $scope.show = false;
                         timeout = $timeout(400)
+                            .then(() => console.log('done hiding, now show'))
                             .then(showNotification)
                             .then(() => $timeout(3000))
+                            .then(() => console.log('done'))
                             .then(hideNotification)
                             .then(finishTimeout);
                     } else {
+                        console.log('timeout was false, showing then hiding');
                         timeout = $timeout(3000).then(hideNotification);
                     }
                 }
