76eb59f3eacead02d4e46300c2852c2c2411a1fc
Devraj Mehta <devm33@gmail.com>
2015-10-03 15:46:31 -0400

    added service to avoid stupid $watching, interestingly arrow function cannot be used for controller function as it messes up `this`, now having an issue with canceling the existing timeout

--- src/common/notification/notification.js
+++ src/common/notification/notification.js
@@ -1,45 +1,53 @@
 angular.module('notification', [])
+    .factory('notification', ($rootScope) => {
+        var $scope = $rootScope.$new();
+        return {
+            notify: () => $scope.$broadcast('notify'),
+            on: (cb) => $scope.$on('notify', cb),
+        };
+    })
     .directive('notification', () => ({
         restrict: 'E',
-        scope: {show: '=', msg: '@'},
+        scope: {msg: '@'},
         templateUrl: 'common/notification/notification.html',
-        controller: ($scope, $timeout) => {
-            var flags = {timeout: false, secondTimeout: false};
-            var showNotification = () => $scope.show = true;
-            var hideNotification = () => $scope.show = false;
-            var setFlagFalse = (flag) => () => flags[flag] = false;
-            var onShow = () => {
-                console.log('show');
-                flags.timeout = $timeout(3000)
-                    .then(setFlagFalse('timeout'))
-                    .then(hideNotification)
-                    .then(() => console.log('show done'));
-            };
-            var onReshow = () => {
-                if(!flags.secondTimeout) {
-                    console.log('reshow');
-                    $timeout.cancel(flags.timeout);
-                    $scope.show = false;
-                    flags.secondTimeout = true;
-                    flags.timeout = $timeout(400)
-                        .then(() => console.log('done hiding, now show'))
-                        .then(showNotification)
-                        .then(() => $timeout(3000))
-                        .then(() => console.log('reshow done'))
-                        .then(hideNotification)
-                        .then(setFlagFalse('secondTimeout'))
-                        .then(setFlagFalse('timeout'));
-                }
-            };
-            $scope.$watch('show', (show) => {
-                console.log('watch triggered\t show =', show, '\ttimeout =', flags.timeout);
-                if(show) {
-                    if(flags.timeout) {
-                        onReshow();
-                    } else {
-                        onShow();
-                    }
-                }
-            });
-        }
+        controllerAs: 'ctrl',
+        controller: notifcationController,
     }));
+
+function notifcationController($timeout, notification) {
+    var flags = {timeout: false, secondTimeout: false};
+    var setShow = (value) => () => this.show = value;
+    var setFlag = (flag, value) => () => flags[flag] = value;
+    var onShow = () => {
+        console.log('show');
+        this.show = true;
+        flags.timeout = $timeout(3000)
+            .then(setFlag('timeout', false))
+            .then(setShow(false))
+            .then(() => console.log('show done'));
+    };
+    var onReshow = () => {
+        if(!flags.secondTimeout) {
+            console.log('reshow');
+            console.log('cancel succesful?', $timeout.cancel(flags.timeout));
+            this.show = false;
+            flags.secondTimeout = true;
+            flags.timeout = $timeout(400)
+                .then(() => console.log('done hiding, now show'))
+                .then(setShow(true))
+                .then(() => $timeout(3000))
+                .then(() => console.log('reshow done'))
+                .then(setShow(false))
+                .then(setFlag('secondTimeout', false))
+                .then(setFlag('timeout', false));
+        }
+    };
+    notification.on(() => {
+        console.log('notification timeout =', flags.timeout);
+        if(flags.timeout) {
+            onReshow();
+        } else {
+            onShow();
+        }
+    });
+}
