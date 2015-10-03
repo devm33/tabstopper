eccea99f8e3871a3b19fa2b6d724939f486348e6
Devraj Mehta <devm33@gmail.com>
2015-10-03 16:07:33 -0400

    remove debug

--- src/common/notification/notification.js
+++ src/common/notification/notification.js
@@ -17,30 +17,24 @@ angular.module('notification', [])
 function notifcationController($timeout, notification) {
     var setThis = (prop, value) => () => this[prop] = value;
     var onShow = () => {
-        console.log('show');
         this.show = true;
         this.timeout = $timeout(3000);
         this.timeout
             .then(setThis('timeout', false))
-            .then(setThis('show', false))
-            .then(() => console.log('show done'));
+            .then(setThis('show', false));
     };
     var onReshow = () => {
-        console.log('reshow');
-        console.log('cancel successful?', $timeout.cancel(this.timeout));
+        $timeout.cancel(this.timeout);
         this.show = false;
         this.secondTimeout = true;
         this.timeout = $timeout(400);
         this.timeout
-            .then(() => console.log('done hiding, now show'))
             .then(setThis('show', true))
             .then(() => this.timeout = $timeout(3000))
-            .then(() => console.log('reshow done'))
             .then(setThis('show', false))
             .then(setThis('timeout', false));
     };
     notification.on(() => {
-        console.log('notification timeout =', this.timeout);
         if(this.timeout) {
             onReshow();
         } else {
