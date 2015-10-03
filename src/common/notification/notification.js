5caf6ffff60d1f1d1327ccdbf99f88d1af9d8f6d
Devraj Mehta <devm33@gmail.com>
2015-10-03 16:09:48 -0400

    consolidate

--- src/common/notification/notification.js
+++ src/common/notification/notification.js
@@ -16,29 +16,20 @@ angular.module('notification', [])
 
 function notifcationController($timeout, notification) {
     var setThis = (prop, value) => () => this[prop] = value;
-    var onShow = () => {
-        this.show = true;
-        this.timeout = $timeout(3000);
-        this.timeout
-            .then(setThis('timeout', false))
-            .then(setThis('show', false));
-    };
-    var onReshow = () => {
-        $timeout.cancel(this.timeout);
-        this.show = false;
-        this.secondTimeout = true;
-        this.timeout = $timeout(400);
-        this.timeout
-            .then(setThis('show', true))
-            .then(() => this.timeout = $timeout(3000))
-            .then(setThis('show', false))
-            .then(setThis('timeout', false));
-    };
     notification.on(() => {
         if(this.timeout) {
-            onReshow();
+            $timeout.cancel(this.timeout);
+            this.show = false;
+            (this.timeout = $timeout(400))
+                .then(setThis('show', true))
+                .then(() => this.timeout = $timeout(3000))
+                .then(setThis('show', false))
+                .then(setThis('timeout', false));
         } else {
-            onShow();
+            this.show = true;
+            (this.timeout = $timeout(3000))
+                .then(setThis('timeout', false))
+                .then(setThis('show', false));
         }
     });
 }
