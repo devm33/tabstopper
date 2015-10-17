8e56f81497a06149eb1b5e82394dbc762571029b
Devraj Mehta <devm33@gmail.com>
2015-10-17 16:08:07 -0400

    tighten notification wait up and use variable

--- src/common/notification/notification.js
+++ src/common/notification/notification.js
@@ -15,6 +15,7 @@ angular.module('notification', [])
     }));
 
 function notifcationController($timeout, notification) {
+    var displayTime = 2000;
     var setThis = (prop, value) => () => this[prop] = value;
     notification.on(() => {
         if(this.timeout) {
@@ -22,12 +23,12 @@ function notifcationController($timeout, notification) {
             this.show = false;
             (this.timeout = $timeout(400))
                 .then(setThis('show', true))
-                .then(() => this.timeout = $timeout(3000))
+                .then(() => this.timeout = $timeout(displayTime))
                 .then(setThis('show', false))
                 .then(setThis('timeout', false));
         } else {
             this.show = true;
-            (this.timeout = $timeout(3000))
+            (this.timeout = $timeout(displayTime))
                 .then(setThis('timeout', false))
                 .then(setThis('show', false));
         }
